291904bb05d98dbf8df7812eb60f308c693c9c11
Kevin Dodokal <dodokalkev@gmail.com>
2026-05-26 16:10:31 +0200

    perf: optimisation images en format WebP

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Feature from "../components/Feature";
-import iconChat from "../assets/img/icon-chat.png";
-import iconMoney from "../assets/img/icon-money.png";
-import iconSecurity from "../assets/img/icon-security.png";
+import iconChat from "../assets/img/icon-chat.webp";
+import iconMoney from "../assets/img/icon-money.webp";
+import iconSecurity from "../assets/img/icon-security.webp";
 
 function Home() {
   return (
