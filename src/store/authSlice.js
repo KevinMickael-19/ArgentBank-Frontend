a7aa701f3f0223d7f2ef468d7ee40110d7d33fb2
Kevin Dodokal <dodokalkev@gmail.com>
2026-05-27 18:27:53 +0200

    feat: ajout authActions et extraReducers dans authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { loginUser } from "./authActions";
 
 const authSlice = createSlice({
   name: "auth",
@@ -9,6 +10,22 @@ const authSlice = createSlice({
     error: null,
   },
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.token = action.payload;
+      })
+
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default authSlice.reducer;
