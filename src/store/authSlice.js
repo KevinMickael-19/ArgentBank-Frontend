6a681d635a0876f7e368bfa1583d0cf318622c45
Kevin Dodokal <dodokalkev@gmail.com>
2026-05-27 17:48:53 +0200

    feat: initialisation Redux store avec authSlice

---
+++ src/store/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    token: null,
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+});
+
+export default authSlice.reducer;
