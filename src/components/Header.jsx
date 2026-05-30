543dadc95ab11eeba5282ae03da78acce724a78d
Kevin Dodokal <dodokalkev@gmail.com>
2026-05-30 13:43:57 +0200

    feat: affichage username dans le Header selon état de connexion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import logo from "../assets/img/argentBankLogo.png";
 
 function Header() {
+  const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
   return (
     <nav className="main-nav">
       <NavLink className="main-nav-logo" to="/">
@@ -13,10 +16,17 @@ function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-        <NavLink className= "main-nav-item" to = "/login"> 
+        {token ? (
+          <NavLink className="main-nav-item" to="/profile">
+            <i className="fa fa-user-circle"></i>
+            {user && user.userName}
+          </NavLink>
+        ) : (
+          <NavLink className="main-nav-item" to="/login">
             <i className="fa fa-user-circle"></i>
             Sign in
-        </NavLink>
+          </NavLink>
+        )}
       </div>
     </nav>
   );
